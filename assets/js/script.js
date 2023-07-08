c3adbb8b0790b79ecee62319468edc8ee1e77c98
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-08 13:50:57 +0400

    javascript

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+//var,let,const
+//variable
+
+//boolean 2 deyer oturu (true,false)
+
+// const name = "Rahide";
+// const nameUniversity = false;
+// const work = true;
+
+// var maas = 900;
+
+// var name2 = "Husniyye";
+// var age = 22;
+// var school = false;
+// let universtiy = true;
+// var maas2 = 950;
+// var macbook = "PC";
+// var gosterici = "3060suret";
+// var totalMaas = maas + maas2;
+
+// console.log(totalMaas);
+
+// let mydata;
