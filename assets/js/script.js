a50095a744dc6c94ec5e46fdbbc4cfdfeabb9330
husniyya05 <husniyyemecnunova@mail.ru>
2023-07-10 19:56:59 +0400

    blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,24 @@
 
 //boolean 2 deyer oturu (true,false)
 
-// const name = "Rahide";
-// const nameUniversity = false;
-// const work = true;
+const name = "Rahide";
+const nameUniversity = false;
+const work = true;
 
-// var maas = 900;
+var maas = 900;
 
-// var name2 = "Husniyye";
-// var age = 22;
-// var school = false;
-// let universtiy = true;
-// var maas2 = 950;
-// var macbook = "PC";
-// var gosterici = "3060suret";
-// var totalMaas = maas + maas2;
+var name2 = "Husniyye";
+var age = 22;
+var school = false;
+let universtiy = true;
+var maas2 = 950;
+var macbook = "PC";
+var gosterici = "3060suret";
+var totalMaas = maas + maas2;
 
-// console.log(totalMaas);
+console.log(totalMaas);
 
-// let mydata;
+let mydata;
+
+var faiz = maas *5/100;
+console.log(faiz);
