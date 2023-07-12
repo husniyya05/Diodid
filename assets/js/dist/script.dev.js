d104e0c79c8b8c45c7cbc64cc4379f8f5e3c2c29
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-12 22:12:31 +0400

    javascript

--- assets/js/dist/script.dev.js
+++ assets/js/dist/script.dev.js
@@ -1,47 +1,36 @@
+"use strict";
+
 //var,let,const
 //variable
-
 //boolean 2 deyer oturu (true,false)
-
-const nameUniversity = false;
-const work = true;
-
+var nameUniversity = false;
+var work = true;
 var maas = 900;
-
 var name2 = "Husniyye";
 var age = 22;
 var school = false;
-let universtiy = true;
+var universtiy = true;
 var maas2 = 950;
 var macbook = "PC";
 var gosterici = "3060suret";
 var totalMaas = maas + maas2;
-
 console.log(totalMaas);
-
-let mydata;
-
-var faiz = (maas * 5) / 100;
+var mydata;
+var faiz = maas * 5 / 100;
 console.log(faiz);
-
 var galass = true;
 var color = "red";
-
-var personals = [
-  {
-    name: "Husniyye",
-    age: 22,
-    tall: 170,
-  },
-  {
-    name: "Rahide",
-    age: 22,
-    tall: 160,
-  },
-  {
-    name: "Aqsin",
-    age: 10,
-    tall: 160,
-  },
-];
+var personals = [{
+  name: "Husniyye",
+  age: 22,
+  tall: 170
+}, {
+  name: "Rahide",
+  age: 22,
+  tall: 160
+}, {
+  name: "Aqsin",
+  age: 10,
+  tall: 160
+}];
 console.log(personals[2].age);
