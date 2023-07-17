180abd8d8f4407c2aba3318871151acf3babd6d0
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-17 21:22:23 +0400

    g

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,69 +3,95 @@
 
 // //boolean 2 deyer oturu (true,false)
 
+// const nameUniversity = false;
 
-const nameUniversity = false;
-const work = true;
+// var maas = 900;
 
-var maas = 900;
+// var name2 = "    Husniyye   ";
 
-var name2 = "Husniyye";
-var age = 22;
-var school = false;
-let universtiy = true;
-var maas2 = 950;
-var macbook = "PC";
-var gosterici = "3060suret";
-var totalMaas = maas + maas2;
+// console.log(name2.trim());
 
-console.log(totalMaas);
+// var age = 22;
+// var school = false;
+// let universtiy = true;
+// var maas2 = 950;
+// var macbook = "PC";
+// var gosterici = "3060suret";
+// var totalMaas = maas + maas2;
 
-let mydata;
+// console.log(totalMaas);
 
-var faiz = (maas * 5) / 100;
-console.log(faiz);
+// let mydata;
 
-var galass = true;
-var color = "red";
+// var faiz = (maas * 5) / 100;
+// console.log(faiz);
 
-var personals = [
-  {
-    name: "Husniyye",
-    age: 22,
-    tall: 170,
-  },
-  {
-    name: "Rahide",
-    age: 22,
-    tall: 160,
-  },
-  {
-    name: "Aqsin",
-    age: 10,
-    tall: 160,
-  },
-];
-console.log(personals[2].age);
+// var galass = true;
+// var color = "red";
 
-var book =[
-    {
-        writer: "Lev Tolstoy",
-        work: "Anna Karenina",
-        year: 1890,
-    },
+// var personals = [
+//   {
+//     name: "Husniyye",
+//     age: 22,
+//     tall: 170,
+//   },
+//   {
+//     name: "Rahide",
+//     age: 22,
+//     tall: 160,
+//   },
+//   {
+//     name: "Aqsin",
+//     age: 10,
+//     tall: 160,
+//   },
+// ];
+// console.log(personals[2].age);
 
-    {
-        writer: "Paulo Coelo",
-        work: "Kimyager",
-        year: 1995,
+// const work = true;
 
-    },
-    {
-        writer:"Franz Kafka",
-        work: "Cevrilme",
-        year: 1910,
+// var book = [
+//   {
+//     writer: "Lev Tolstoy",
+//     work: "Anna Karenina",
+//     year: 1890,
+//   },
 
-    },
-];
-console.log(book[1].work);
+//   {
+//     writer: "Paulo Coelo",
+//     work: "Kimyager",
+//     year: 1995,
+//   },
+//   {
+//     writer: "Franz Kafka",
+//     work: "Cevrilme",
+//     year: 1910,
+//   },
+// ];
+// console.log(book);
 
+// const d = new Date("2022-03-25");
+// console.log(d);
+
+// const arryData = ["Dostoyevski", "Tolstoy", "Frans Kafka"];
+
+// console.log(arryData);
+
+// arryData.unshift("lIMON");
+
+// console.log(arryData);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// const sortMethod = fruits.sort();
+
+// console.log(sortMethod);
+
+const age = 17;
+let system;
+
+if (age > 18) {
+  system = true;
+} else {
+  system = false;
+}
